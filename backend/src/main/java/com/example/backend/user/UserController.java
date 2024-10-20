9c760b1d72130a252a1f9279216868c177c53b2c
Vicente Marques <vicente.marques@tecnico.ulisboa.pt>
2024-10-20 11:05:25 +0100

    user register

---
+++ backend/src/main/java/com/example/backend/user/UserController.java
@@ -0,0 +1,28 @@
+package com.example.backend.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<String> registerUser(@RequestBody UserRegistrationRequest request) {
+        userService.registerUser(request.getUsername(), request.getPassword(), request.getEmail());
+        return ResponseEntity.ok("User registered successfully");
+    }
+
+    // add login
+}
